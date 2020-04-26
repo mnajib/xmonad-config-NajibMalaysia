9e78f08ce9db1ef5c0dfeeb2cca1648f011bbe80
Najib <mnajib@gmail.com>
2020-04-26 22:55:00 +0800

    Create fifo and run zikir for top xmobar panel start script

--- run-xmobar-top.sh
+++ run-xmobar-top.sh
@@ -1,6 +1,11 @@
 #!/run/current-system/sw/bin/env bash
 
+mkfifo /tmp/najib-wsp.log
+mkfifo /tmp/najib-zikirpipe
+
 #trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x191970 --height 12 &
 trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x181715 --height 12 --alpha 0 &
+
+~/.xmonad/zikir &
 
 xmobar ~/.xmonad/xmobarrc-top.hs &
