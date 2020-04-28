c8136e77b25f263c0b11b11661c64902f97dd8b1
Najib <mnajib@gmail.com>
2020-04-29 03:05:42 +0800

    Minor cosmetic change for top bar

--- run-xmobar-top.sh
+++ run-xmobar-top.sh
@@ -8,7 +8,8 @@ mkfifo /tmp/najib-zikirpipe
 
 #trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x191970 --height 12 &
 #trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x181715 --height 12 --alpha 0 &
-trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 15 --transparent true --tint 0x555555 --height 14 --alpha 0 &
+#trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 15 --transparent true --tint 0x555555 --height 14 --alpha 0 &
+trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 12 --transparent true --tint 0x555555 --height 14 --alpha 0 &
 
 ~/.xmonad/zikir &
 pasystray &
