933c156b9000b80aee859860a44d02ee55beff07
Najib <mnajib@gmail.com>
2020-04-28 19:00:45 +0800

    Increase height, width and brighter background color for trayer

--- run-xmobar-top.sh
+++ run-xmobar-top.sh
@@ -1,11 +1,16 @@
 #!/run/current-system/sw/bin/env bash
 
-mkfifo /tmp/najib-wsp.log
+#mkfifo /tmp/najib-wsp.log
+# Reset the log file
+cat /dev/null > /tmp/najib-wsp.log
+
 mkfifo /tmp/najib-zikirpipe
 
 #trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x191970 --height 12 &
-trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x181715 --height 12 --alpha 0 &
+#trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x181715 --height 12 --alpha 0 &
+trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 15 --transparent true --tint 0x555555 --height 14 --alpha 0 &
 
 ~/.xmonad/zikir &
-
+pasystray &
+~/.fehbg &
 xmobar ~/.xmonad/xmobarrc-top.hs &
