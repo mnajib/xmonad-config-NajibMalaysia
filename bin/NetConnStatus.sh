3170b7210f51e90a3dfa720a3202940762de16e9
Najib <mnajib@gmail.com>
2021-05-24 21:11:53 +0800

    Bug LAN WAN network status fixed.

--- bin/NetConnStatus.sh
+++ bin/NetConnStatus.sh
@@ -6,46 +6,27 @@
 
 ping_interface() {
     # Max value of losted packages in %
-    MAX_PACKETS_LOST=80
-    PACKETS_COUNT=10
+    PACKETS_LOST_TRESHOLD=80
+    PACKETS_COUNT=3 #10
+    PACKETS_LOST=0
 
-    #PACKETS_LOST=$(ping -c $PACKETS_COUNT -I $1 $2 |grep % | awk '{print $7}')
-    #PACKETS_LOST=$(ping -c $PACKETS_COUNT -I $1 $2 | grep '% packet loss' | awk '{print $6}')
-    PACKETS_LOST=$(ping -c $PACKETS_COUNT $2 | grep '% packet loss' | awk '{print $6}')
+    PACKETS_LOST=$(ping -q -c $PACKETS_COUNT $2 2> /dev/null | grep '% packet loss' | sed 's/, /\n/g' | grep 'loss' | sed 's/\%.*$//g')
 
-    #if ! [ -n "$PACKETS_LOST" ] || [ "$PACKETS_LOST" == "100%" ];
-    if [ "$PACKETS_LOST" == "100%" ];
-    then
-        # 100% failed
-        #echo "XXX1: 100% FAILED. Return 1."
-        echo "1"
+    if [ "$PACKETS_LOST" == "" ]; then
+	#echo "FAILED. Return 1."
         return 1
     else
-        if [ "${PACKETS_LOST}" == "0%" ];
-        then
-            #ping is OK
-            #echo "XXX2: Ping is OK. Return 0."
-            echo "0"
-            return 0
-        else # If not 100% and not 0%, ...
-            # Real value of losted packets between 0 and 100%
-            REAL_PACKETS_LOST=$(echo $PACKETS_LOST | sed 's/.$//')
-            if [[ ${REAL_PACKETS_LOST} -gt ${MAX_PACKETS_LOST} ]]; # ...but too much packets loss
-            then
-                #echo "Network connection status: Failed. Lost more then limit."
-                #echo "XXX3: Network Connection is FAILED with ${PACKETS_LOST} packets lost. Return 1."
-                echo "1"
-                return 1
-            else # ...but with just a little packets loss
-                #echo "Network connection status: OK."
-                #echo "XXX4: Network connection is still OK with ${PACKETS_LOST} packets lost. Return 0."
-                echo "0"
-                return 0
-            fi
-        fi
+	if (( ${PACKETS_LOST} > ${PACKETS_LOST_TRESHOLD} )); then
+                #echo "Network Connection is FAILED with ${PACKETS_LOST} packets lost. Return 2."
+		#echo 1
+                return 2
+	else
+		#echo "Network is fine. Return 0."
+		return 0
+	fi
     fi
 }
 
 #ping_interface enp7s0 192.168.1.1
 #ping_interface enp7s0 1.1.1.1
-#ping_interface ${1} ${2}
+ping_interface ${1} ${2}
--- netconnstatus.sh
+++ netconnstatus.sh
@@ -1,39 +1,42 @@
 #!/run/current-system/sw/bin/env bash
 
-. ~/.xmonad/bin/NetConnStatus.sh
+#. ~/.xmonad/bin/NetConnStatus.sh
+PING=~/.xmonad/bin/NetConnStatus.sh
 
 s=""
 FgColor1="#181715"
 BgColor1="#58C5F1"
 FgColor2="#646464"
 BgColor2="#ff0000"
-#timeInterval=`echo "60 * 2" | bc` # every 2 minutes
-timeInterval=60
+timeInterval=10 #15 60
 
 while true; do
 	#date
 	s=""
-	pingLAN=$(ping_interface enp7s0 192.168.123.1)
+
+	~/.xmonad/bin/NetConnStatus.sh enp7s0 192.168.123.1
+	pingLAN=$?
 	sleep 1
-	#pingLAN2=$(ping_interface enp7s0 192.168.123.77)
-	pingWAN=$(ping_interface enp7s0 1.1.1.1)
+
+	~/.xmonad/bin/NetConnStatus.sh enp7s0 1.1.1.1 2> /dev/null
+	pingWAN=$?
 	sleep 1
 
 	# Check LAN
 	if [ $pingLAN -eq 0 ]; then
-		s+="<fc=${FgColor1},${BgColor1}>LAN</fc>"
+		s+="<fc=${FgColor1},${BgColor1}> LAN</fc>"
 		#echo "LAN: OK"
 	else
-		s+="<fc=${FgColor2},${BgColor2}>LAN</fc>"
+		s+="<fc=${FgColor2},${BgColor2}> LAN</fc>"
 		#echo "LAN: KO"
 	fi
 
 	# Check WAN
 	if [ $pingWAN -eq 0 ]; then
-		s+=",<fc=${FgColor1},${BgColor1}>WAN</fc>"
+		s+=",<fc=${FgColor1},${BgColor1}> WAN</fc>"
 		#echo "WAN: OK"
 	else
-		s+=",<fc=${FgColor2},${BgColor2}>WAN</fc>"
+		s+=",<fc=${FgColor2},${BgColor2}> WAN</fc>"
 		#echo "WAN: KO"
 	fi
 
