77cfd8e5226099f94e8d3f306e94eefab7bbc0fe
Najib <mnajib@gmail.com>
2021-05-31 16:04:32 +0800

    Lower the LAN/WAN check interval.

--- netconnstatus.sh
+++ netconnstatus.sh
@@ -8,7 +8,7 @@ FgColor1="#181715"
 BgColor1="#58C5F1"
 FgColor2="#646464"
 BgColor2="#ff0000"
-timeInterval=10 #15 60
+timeInterval=2 #5 #10 #15 60
 
 while true; do
 	#date
@@ -16,11 +16,11 @@ while true; do
 
 	~/.xmonad/bin/NetConnStatus.sh enp7s0 192.168.123.1
 	pingLAN=$?
-	sleep 1
+	#sleep 1
 
 	~/.xmonad/bin/NetConnStatus.sh enp7s0 1.1.1.1 2> /dev/null
 	pingWAN=$?
-	sleep 1
+	#sleep 1
 
 	# Check LAN
 	if [ $pingLAN -eq 0 ]; then
