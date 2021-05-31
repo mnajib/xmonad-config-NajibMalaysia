e416a869b0cd9adc2f1ba602e687aa8f53f2fd4a
Najib <mnajib@gmail.com>
2021-05-31 17:39:11 +0800

    Some code cleanup on netconnstatus.sh

--- netconnstatus.sh
+++ netconnstatus.sh
@@ -1,47 +1,47 @@
 #!/run/current-system/sw/bin/env bash
 
-#. ~/.xmonad/bin/NetConnStatus.sh
 PING=~/.xmonad/bin/NetConnStatus.sh
+#LOGFILE=~/.xmonad/var/${USER}-NetConnStatus.log # date_time, LAN_status, WAN_status
 
-s=""
 FgColor1="#181715"
 BgColor1="#58C5F1"
 FgColor2="#646464"
 BgColor2="#ff0000"
 timeInterval=2 #5 #10 #15 60
 
+function printStatus {
+	case $1 in
+		1)
+			echo "<fc=${FgColor1},${BgColor1}> LAN</fc>,<fc=${FgColor1},${BgColor1}> WAN</fc>"
+			;;
+		2)
+			echo "<fc=${FgColor2},${BgColor2}> LAN</fc>,<fc=${FgColor1},${BgColor1}> WAN</fc>"
+			;;
+		3)
+			echo "<fc=${FgColor1},${BgColor1}> LAN</fc>,<fc=${FgColor2},${BgColor2}> WAN</fc>"
+			;;
+		4)
+		echo "<fc=${FgColor2},${BgColor2}> LAN</fc>,<fc=${FgColor2},${BgColor2}> WAN</fc>"
+			;;
+	esac
+}
+
 while true; do
-	#date
-	s=""
-
-	~/.xmonad/bin/NetConnStatus.sh enp7s0 192.168.123.1
+	${PING} enp7s0 192.168.123.1 2> /dev/null
 	pingLAN=$?
-	#sleep 1
 
-	~/.xmonad/bin/NetConnStatus.sh enp7s0 1.1.1.1 2> /dev/null
+	${PING} enp7s0 1.1.1.1 2> /dev/null
 	pingWAN=$?
-	#sleep 1
 
-	# Check LAN
-	if [ $pingLAN -eq 0 ]; then
-		s+="<fc=${FgColor1},${BgColor1}> LAN</fc>"
-		#echo "LAN: OK"
-	else
-		s+="<fc=${FgColor2},${BgColor2}> LAN</fc>"
-		#echo "LAN: KO"
+	if [ $pingLAN -eq 0 ] && [ $pingWAN -eq 0 ]; then
+		printStatus 1
+	elif [ $pingLAN -ne 0 ] && [ $pingWAN -eq 0 ]; then
+		printStatus 2
+	elif [ $pingLAN -eq 0 ] && [ $pingWAN -ne 0 ]; then
+		printStatus 3
+	elif [ $pingLAN -ne 0 ] && [ $pingWAN -ne 0 ]; then
+		printStatus 4
 	fi
-
-	# Check WAN
-	if [ $pingWAN -eq 0 ]; then
-		s+=",<fc=${FgColor1},${BgColor1}> WAN</fc>"
-		#echo "WAN: OK"
-	else
-		s+=",<fc=${FgColor2},${BgColor2}> WAN</fc>"
-		#echo "WAN: KO"
-	fi
-
-	echo "${s}"
-	#date
 
 	sleep ${timeInterval}
 done
