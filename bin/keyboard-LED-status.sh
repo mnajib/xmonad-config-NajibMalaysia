4a4b5f39ffb718060dc20c9e6d592c457c74f024
Najib Ibrahim <mnajib@gmail.com>
2024-01-09 13:31:50 +0800

    sleep 1 to 2 for keyboard LED status

--- bin/keyboard-LED-status.sh
+++ bin/keyboard-LED-status.sh
@@ -11,7 +11,7 @@ while true; do
     s=`xset -q | egrep '[0-9][0-9]:' | sed 's/[0-9][0-9]: /\n/g' | sed 's/\n//g' | grep 'Lock:' | sed 's/ Lock:/:/g' | sed 's/\ *//g' | sed -e "/on/ s,^,<fc=${onColor}>,; /off/ s,^,<fc=${offColor}>,; s,:.*$,<\/fc>," | tr '\n' ' ' | sed 's/$/\n/g'`
 
     echo "$s"
-    sleep 1
+    sleep 2 #1
 done
 
 # XXX: TODO?
