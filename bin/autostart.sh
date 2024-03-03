d2a39e97fae52d4dd3a568c1f528f8f5a48ccd2b
Najib Ibrahim <mnajib@gmail.com>
2024-03-04 01:03:17 +0800

    rename some files related to screen-layout

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -5,58 +5,49 @@
 case $HOSTNAME in
   keira)
     echo "Running some customization for $HOSTNAME"
-    # If VGA-1 connected, do
-    # setting for dual monitor
-    #$HOME/.xmonad/bin/keira-dual-screen-dell-monitor.sh dual
-    $HOME/.xmonad/bin/screenlayout.khadijah.sh
-    # else do
-    # setting for single monitor
-    # ...
+    $HOME/.xmonad/bin/screenlayout-khadijah.sh
     sleep 1
     setxkbmap us
     ;;
   zahrah)
     echo "Running some customization for $HOSTNAME"
-    ##$HOME/.xmonad/bin/init-secondMonitorPhilips1280x1024-forZahrah.sh dual
-    #$HOME/.xmonad/bin/zahrah-dual-screen-philips-monitor.sh dual
-    ##$HOME/.xmonad/bin/zahrah-dual-screen-philips-monitor.sh single
     $HOME/.xmonad/bin/screenlayout-zahrah.sh
+    sleep 1
+    setxkbmap dvorak
+    ;;
+  khawlah)
+    echo "Running some customization for $HOSTNAME"
+    $HOME/.xmonad/bin/screenlayout-khawlah.sh
     sleep 1
     setxkbmap dvorak
     ;;
   sakinah)
     echo "Running some customization for $HOSTNAME"
-    $HOME/.xmonad/bin/sakinah-dual-screen-dell-monitor.sh dual
+    #$HOME/.xmonad/bin/sakinah-dual-screen-dell-monitor.sh dual
     sleep 1
     setxkbmap dvorak
     ;;
   asmak|naqib)
     echo "Running some customization for $HOSTNAME"
-    #$HOME/.xmonad/bin/asmak-dualMonitor-ThinkpadT430s_1600x900-ThinkVision1280x1024.sh
-    #$HOME/.xmonad/bin/asmak-dualMonitor-ThinkpadT430s_1600x900-Dell_P1913S_1280x1024.sh
-    #$HOME/.xmonad/bin/asmak_with_vga_PanasonicTV_1024x768.sh dual
-    $HOME/.xmonad/bin/screenlayout.asmak.sh
+    $HOME/.xmonad/bin/screenlayout-asmak.sh
     sleep 1
     setxkbmap dvorak
     ;;
   raudah)
     echo "Running some customization for $HOSTNAME"
-    $HOME/.xmonad/bin/screenlayout.raudah.sh
+    $HOME/.xmonad/bin/screenlayout-raudah.sh
     sleep 1
     setxkbmap dvorak
     ;;
   delldesktop)
     echo "Running some customization for $HOSTNAME"
-    $HOME/.xmonad/bin/delldesktop-dualMonitor-ThinkVision1280x1024-PanasonicTV1920x1080.sh
+    #$HOME/.xmonad/bin/delldesktop-dualMonitor-ThinkVision1280x1024-PanasonicTV1920x1080.sh
     sleep 1
     setxkbmap dvorak
     ;;
   khadijah)
     echo "Running some customization for $HOSTNAME"
-    #$HOME/.xmonad/bin/delldesktop-dualMonitor-ThinkVision1280x1024-PanasonicTV1920x1080.sh
-    #$HOME/.screenlayout/host-khadijah-with2externalDellMonitor-122.sh
-    #$HOME/.xmonad/bin/host-khadijah-with2externalDellMonitor-122.sh
-    $HOME/.xmonad/bin/screenlayout.khadijah.sh
+    $HOME/.xmonad/bin/screenlayout-khadijah.sh
     sleep 1
     setxkbmap dvorak
     ;;
