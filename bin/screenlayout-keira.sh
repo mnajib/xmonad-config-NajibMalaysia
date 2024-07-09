7029a2991c7e44e5ce253e54513876832ab96c49
Juliani Jaffar <juliani.jaffar@gmail.com>
2024-07-09 13:17:36 +0800

    screenlayout-keira.sh

--- bin/screenlayout-keira.sh
+++ bin/screenlayout-keira.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS-1 --primary --mode 1440x900 --pos 0x480 --rotate normal --output VGA-1 --mode 1280x1024 --pos 1752x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output HDMI-3 --off --output DP-2 --off --output DP-3 --off
+xrandr --output LVDS-1 --primary --mode 1440x900 --pos 0x589 --rotate normal --output VGA-1 --mode 1280x1024 --pos 1440x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output HDMI-3 --off --output DP-2 --off --output DP-3 --off
