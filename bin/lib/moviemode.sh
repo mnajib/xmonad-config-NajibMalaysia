554075c5b6134edbef1306a96165586c373595c0
Najib Ibrahim <mnajib@gmail.com>
2025-10-31 21:32:14 +0800

    lib/moviemode.sh: fix logic error

--- bin/lib/moviemode.sh
+++ bin/lib/moviemode.sh
@@ -14,12 +14,18 @@ offColor="${FgColor2}"
 
 #dpms_state=$(xset q | grep "DPMS is" | awk '{print $3}')
 
-set_moviemode_off() {
+set_moviemode_on() {
+    # Disables both screen blanking and Display Power Management Signaling (DPMS)
+    #   xset s off: Turns off the screen saver (prevents blanking due to inactivity).
+    #   xset -dpms: Disables monitor power-saving features (no standby/suspend/off modes)
     xset s off && xset -dpms
     echo "<action=\`~/.xmonad/bin/toggle-screen-sleep-movie-mode-v2.sh\`><fc=${onColor}>Movie</fc></action>" > "$status_file"  # Red highlight
 }
 
-set_moviemode_on() {
+set_moviemode_off() {
+    # Re-enables both screen blanking and DPMS:
+    #   xset s on: Turns the screen saver back on.
+    #   xset +dpms: Enables monitor power-saving features.
     local status_file="$1"
     xset s on && xset +dpms
     echo "<action=\`~/.xmonad/bin/toggle-screen-sleep-movie-mode-v2.sh\`><fc=${offColor}>Movie</fc></action>" > "${status_file}"    # Green highlight
@@ -29,10 +35,10 @@ toggle_moviemode() {
     local status_file="$1"
     local dpms_state="$2"
 
-    if [[ "$dpms_state" == "Enabled" ]]; then
-      set_moviemode_off "$status_file"
-    else
+    if [[ "$dpms_state" == "Enabled" ]]; then # if movie mode = disable
       set_moviemode_on "$status_file"
+    else
+      set_moviemode_off "$status_file"
     fi
 }
 
