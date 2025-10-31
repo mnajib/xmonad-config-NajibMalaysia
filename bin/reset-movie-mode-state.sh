3abcb5cf8d722a04302d01f746ef3d97b195bbe4
Najib Ibrahim <mnajib@gmail.com>
2025-10-31 21:06:03 +0800

    movie-mode: fix startup and toggle

--- bin/reset-movie-mode-state.sh
+++ bin/reset-movie-mode-state.sh
@@ -3,6 +3,8 @@
 source "${HOME}/.xmonad/bin/lib/moviemode.sh"
 
 set_moviemode_on "$status_file"
+sleep 1
+set_moviemode_off "$status_file"
 
 # Notify XMobar to refresh (if needed)
 #pkill -SIGUSR1 xmobar
