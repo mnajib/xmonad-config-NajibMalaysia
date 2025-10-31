492cb6a38801f78e3ef2e8b3f373223848cb597a
Najib Ibrahim <mnajib@gmail.com>
2025-10-31 21:06:03 +0800

    movie-mode: fix startup and toggle

--- bin/lib/moviemode.sh
+++ bin/lib/moviemode.sh
@@ -3,8 +3,8 @@
 status_file="/tmp/${USER}-movie-mode-status"
 
 # UP
-FgColor1="#000000"  #"#181715"
-BgColor1="#ff0000" #"#58C5F1"
+FgColor1="#000000"
+BgColor1="#ff0000"
 # DOWN
 FgColor2="#646464"
 BgColor2="#ff0000"
@@ -14,6 +14,17 @@ offColor="${FgColor2}"
 
 #dpms_state=$(xset q | grep "DPMS is" | awk '{print $3}')
 
+set_moviemode_off() {
+    xset s off && xset -dpms
+    echo "<action=\`~/.xmonad/bin/toggle-screen-sleep-movie-mode-v2.sh\`><fc=${onColor}>Movie</fc></action>" > "$status_file"  # Red highlight
+}
+
+set_moviemode_on() {
+    local status_file="$1"
+    xset s on && xset +dpms
+    echo "<action=\`~/.xmonad/bin/toggle-screen-sleep-movie-mode-v2.sh\`><fc=${offColor}>Movie</fc></action>" > "${status_file}"    # Green highlight
+}
+
 toggle_moviemode() {
     local status_file="$1"
     local dpms_state="$2"
@@ -21,19 +32,8 @@ toggle_moviemode() {
     if [[ "$dpms_state" == "Enabled" ]]; then
       set_moviemode_off "$status_file"
     else
-      set_moviemode_of "$status_file"
+      set_moviemode_on "$status_file"
     fi
-}
-
-set_moviemode_off() {
-    xset s off && xset -dpms
-    echo "<fc=${onColor}>Movie</fc>" > "$status_file"  # Red highlight
-}
-
-set_moviemode_on() {
-    local status_file="$1"
-    xset s on && xset +dpms
-    echo "<action=\`~/.xmonad/bin/toggle-screen-sleep-movie-mode-v2.sh\`><fc=${offColor}>Movie</fc></action>" > "${status_file}"    # Green highlight
 }
 
 #toggle_moviemode "$status_file" "$dpms_state"
