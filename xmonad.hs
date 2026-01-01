c8e2da8853aa5ece9e80e76e49d1799f3cd2ace3
Najib Ibrahim <mnajib@gmail.com>
2026-01-01 08:12:15 +0800

    increase padding for temporary window zoom, from 3 to 5

--- xmonad.hs
+++ xmonad.hs
@@ -851,7 +851,8 @@ myLayout =
      -- To be use by maximizeWithPadding
      -- myMaxWithPad = 7
      --myMaxWithPad = 1 -- good for testing
-     myMaxWithPad = 3
+     --myMaxWithPad = 3
+     myMaxWithPad = 5
      -- myMaxWithPad = 0.05 -- problem
      -- myMaxWithPad = 0 -- good
 
