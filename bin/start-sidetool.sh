c042cd426764a8b3fbb88ce06bf51f9d3fa2a51f
Najib Ibrahim <mnajib@gmail.com>
2026-05-12 21:08:40 +0800

    waktusolat: fix various parts due to changes on upstream data source

--- bin/start-sidetool.sh
+++ bin/start-sidetool.sh
@@ -204,7 +204,8 @@ nm-applet & # Not really needed, just use nmtui.
 
 #qtox &
 
-$HOME/.xmonad/bin/waktusolat-hbar SGR01 &
+#$HOME/.xmonad/bin/waktusolat-hbar SGR01 &
+$HOME/.xmonad/bin/waktusolat -o hbar SGR01 &
 
 #xscreensaver -no-splash &
 $HOME/.xmonad/bin/set-display-screen-power-saver.sh
