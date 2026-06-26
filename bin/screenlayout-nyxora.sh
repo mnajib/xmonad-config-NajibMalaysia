eb65715eef769eeb93c9c1fbf498be75cf092b33
Najib Ibrahim <mnajib@gmail.com>
2026-06-27 02:01:11 +0800

    screenlayout-nyxora.sh

--- bin/screenlayout-nyxora.sh
+++ bin/screenlayout-nyxora.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-0 --off --output DVI-I-1 --off --output DP-0 --off --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+xrandr --output DisplayPort-0 --mode 2560x1440 --pos 2560x0 --rotate normal --output DisplayPort-1 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-A-0 --off
