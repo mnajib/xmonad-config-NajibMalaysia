78b749adf18a86cc2a1494182b475c3522ec477a
Najib Ibrahim <mnajib@gmail.com>
2026-06-29 10:24:24 +0800

    fix monitor-solat-processes.sh

--- bin/monitor-solat-processes.sh
+++ bin/monitor-solat-processes.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "solat:"
-ps auxwww | grep -v nvim | grep -v grep | grep -v "ps auxwww" | grep -i "solat"
+ps auxwww | grep -v nvim | grep -v grep | grep -v "ps auxwww" | grep -v monitor-solat-process | grep -i "solat"
 echo ""
 echo "xmobar:"
-ps auxwww | grep -v nvim | grep -v grep | grep -v "ps auxwww" | grep -i "solat"
+ps auxwww | grep -v nvim | grep -v grep | grep -v "ps auxwww" | grep -v monitor-solat-process | grep -i "xmobar"
