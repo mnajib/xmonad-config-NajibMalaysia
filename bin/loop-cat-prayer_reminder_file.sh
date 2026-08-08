a95dfed4121409f9aca3dd7c9d8dbcd1c1473396
Najib Ibrahim <mnajib@gmail.com>
2026-08-08 23:00:54 +0800

    Move prayer reminder file from /tmp/ to /run/

--- bin/loop-cat-prayer_reminder_file.sh
+++ bin/loop-cat-prayer_reminder_file.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-PRAYER_REMINDER_FILE="/tmp/${USER}-prayer_reminder_file"
+#PRAYER_REMINDER_FILE="/tmp/${USER}-prayer_reminder_file"
+PRAYER_REMINDER_FILE="/run/user/${UID}/waktusolat/prayer_reminder.xmobar"
+#PRAYER_REMINDER_FILE="/run/waktusolat/reminder.txt"
 
 while true; do
     cat ${PRAYER_REMINDER_FILE}
