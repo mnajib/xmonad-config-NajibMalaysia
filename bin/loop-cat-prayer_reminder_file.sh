ffe554996ebf6740ec2fcf279422289cdb608bde
Najib Ibrahim <mnajib@gmail.com>
2026-08-12 12:01:59 +0800

    display both waktu solat (xmonad project and waktusolat project) in one line for testing the new, need wide monitor

--- bin/loop-cat-prayer_reminder_file.sh
+++ bin/loop-cat-prayer_reminder_file.sh
@@ -2,9 +2,16 @@
 
 #PRAYER_REMINDER_FILE="/tmp/${USER}-prayer_reminder_file"
 PRAYER_REMINDER_FILE="/run/user/${UID}/waktusolat/prayer_reminder.xmobar"
-#PRAYER_REMINDER_FILE="/run/waktusolat/reminder.txt"
+PRAYER_REMINDER_FILE2="/run/waktusolat/reminder.txt"
 
 while true; do
-    cat ${PRAYER_REMINDER_FILE}
+
+    #cat ${PRAYER_REMINDER_FILE}
+
+    # TEST:
+    #cat /run/user/1001/waktusolat/prayer_reminder.xmobar /run/waktusolat/reminder.txt | tr -d '\n'
+    cat ${PRAYER_REMINDER_FILE} ${PRAYER_REMINDER_FILE2} | tr -d '\n'
+    echo
+
     sleep 1
 done
