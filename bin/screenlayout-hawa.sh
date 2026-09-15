4cbcf380f77d2105537a1a8c1d34c76fc481c88b
Najib Ibrahim <mnajib@gmail.com>
2026-09-15 14:19:27 +0800

    maryam (T61/R61), hawa (T61/R61 with broken screen): update ./bin/screenlayout-<hostname>.sh

---
+++ bin/screenlayout-hawa.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS-1 --primary --mode 1280x720 --pos 0x0 --rotate normal --output VGA-1 --off --output DVI-D-1 --off
--- bin/screenlayout-maryam.sh
+++ bin/screenlayout-maryam.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS-1 --primary --mode 1280x720 --pos 0x0 --rotate normal --output VGA-1 --off --output DVI-D-1 --off
+xrandr --output LVDS-1 --primary --mode 1280x800 --pos 0x0 --rotate normal --output VGA-1 --off --output DVI-D-1 --off
